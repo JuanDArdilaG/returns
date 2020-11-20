8c68ee05ce7a347c1c915189cba90a9d4aa6485a
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2020-11-20 16:46:20 -0500

    sync

---
+++ Response/Response.ts
@@ -0,0 +1,73 @@
+import { IError } from "./Failed";
+
+interface IResponse {
+  data: any;
+  errors: Array<IError>;
+  metadata: any;
+  hasErrors: () => boolean;
+}
+
+class Response {
+  static mongooseerrors = {
+    "11000": "duplicate key error",
+  };
+
+  // Variables used to store the errors and the metadata before success response
+  private static _tempErrors: Array<IError> = new Array<IError>();
+  private static _tempMetaData: any;
+
+  static error(error: IError): void {
+    this._tempErrors.push(error);
+  }
+
+  static addMeta(key: string | number, value: any) {
+    this._tempMetaData[key] = value;
+  }
+
+  static failed(finalError: IError | null = null): IResponse {
+    if (finalError) {
+      this.error(finalError);
+    }
+    const { tempE } = this.cleanup();
+
+    return {
+      data: null,
+      errors: tempE,
+      metadata: null,
+      hasErrors: (): boolean => tempE.length > 0,
+    };
+  }
+
+  //**
+  // Function used to return Response Object and cleanup temp variables
+  //**
+  static success(data: any): IResponse {
+    const { tempE, tempM } = this.cleanup();
+    return {
+      data,
+      errors: tempE,
+      metadata: tempM,
+      hasErrors: (): boolean => tempE.length > 0,
+    };
+  }
+
+  private static cleanup() {
+    const tempE = this._tempErrors;
+    const tempM = this._tempMetaData;
+    this._tempErrors = new Array<IError>();
+    this._tempMetaData = {};
+
+    return {
+      tempE,
+      tempM,
+    };
+  }
+}
+
+export default Response;
+export { IResponse };
+
+interface IMongooseError {
+  name: string;
+  code: string | number;
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+import Response from "./Response/Response";
+
+export default class Returns {
+  static success(data: any, errors: Array<Object>, metadata: Object = {}) {}
+}
+
+export { Response };
